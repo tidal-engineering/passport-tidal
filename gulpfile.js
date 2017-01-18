2802f8624e8dab24acf25156704b547885b9d9b2
Nick Fytros <nikolaos.fytros@tidal.com>
2017-01-18 14:46:17 +0100

    fixed eslint errors

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var isparta = require('isparta');
 require('babel-register');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
+    return gulp.src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
@@ -24,55 +24,55 @@ gulp.task('static', function () {
 });
 
 gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
+    nsp({package: path.resolve('package.json')}, cb);
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
+    return gulp.src('lib/**/*.js')
     .pipe(excludeGitignore())
     .pipe(istanbul({
-      includeUntested: true,
-      instrumenter: isparta.Instrumenter
+        includeUntested: true,
+        instrumenter: isparta.Instrumenter
     }))
     .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
+    var mochaErr;
 
-  gulp.src('test/**/*.js')
+    gulp.src('test/**/*.js')
     .pipe(plumber())
     .pipe(mocha({reporter: 'spec'}))
     .on('error', function (err) {
-      mochaErr = err;
+        mochaErr = err;
     })
     .pipe(istanbul.writeReports())
     .on('end', function () {
-      cb(mochaErr);
+        cb(mochaErr);
     });
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
+    gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
 });
 
 gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
+    if (!process.env.CI) {
+        return;
+    }
 
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
     .pipe(coveralls());
 });
 
 gulp.task('babel', ['clean'], function () {
-  return gulp.src('lib/**/*.js')
+    return gulp.src('lib/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
-  return del('dist');
+    return del('dist');
 });
 
 gulp.task('prepublish', ['nsp', 'babel']);
