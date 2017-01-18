ed15c616e65472c0585b237018b2b6d755af9904
Daniel Cherubini <daniel@cherubini.casa>
2017-01-18 14:17:07 +0100

    Re-writing base

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import passportTidal from '../lib';
-
-describe('passport-tidal', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
-});
