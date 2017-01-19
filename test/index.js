44f9e92048dd7094c14f08eadf29d4e4ed8811d3
Daniel Cherubini <daniel@cherubini.casa>
2017-01-19 16:53:40 +0100

    moved unit tests to old, and ready for new unit tests

---
+++ test/index.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('passport-tidal', function () {
+    it('It should have unit tests', function () {
+        assert(true, 'It will have unit tests!');
+    });
+});
